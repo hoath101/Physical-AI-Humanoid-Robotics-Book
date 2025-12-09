46062b753db5ab36ec0a5d9cca92a63ce276fd05
Hammad hoath <hammadhoath123@gmail.com>
2025-12-09 21:28:29 +0500

    removed unnecessary files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\Baloch\\GIAIC-WORK\\hackathon-book\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\Baloch\\GIAIC-WORK\\hackathon-book\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\Baloch\\GIAIC-WORK\\hackathon-book\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\Baloch\\GIAIC-WORK\\hackathon-book\\src\\css\\custom.css"),
-];
